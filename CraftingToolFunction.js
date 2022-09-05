2b9b2a0d9c59720166fcaaeea5020cae50137f35
iDarkStar29 <111619558+iDarkStar29@users.noreply.github.com>
2022-09-05 12:42:11 -0500

    Update CraftingToolFunction.js

--- CraftingToolFunction.js
+++ CraftingToolFunction.js
@@ -8,3 +8,10 @@ btnGet.addEventListener('click', () => {
 
 
 });
+// auto-table by variable
+let text = "";
+
+for (let i = 0; i < 26; i++) {
+  text += "<tr><td>" + perkName[i] + "</td><td>" + perkEffect[i] + "</td><td>" + perkPercent[i] + "</td></tr>";
+}
+document.getElementById("myTable").innerHTML = text;
