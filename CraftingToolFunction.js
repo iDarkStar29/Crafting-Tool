4adb8b5ae68f5818ab43f9624228ce08f8962140
iDarkStar29 <111619558+iDarkStar29@users.noreply.github.com>
2022-09-07 20:13:24 -0500

    Update CraftingToolFunction.js

--- CraftingToolFunction.js
+++ CraftingToolFunction.js
@@ -1,41 +1,68 @@
-let btnGet = document.querySelector('button');
-let myTable = document.querySelector('#table');
-
-
-
-
-btnGet.addEventListener('click', () => {
-
-
-});
-// auto-table by array
-<script>
 let text = "";
-
-for (let i = 0; i < 26; i++) {
-  text += 
+let button = "";
+for (let i = 0; i < 26; i++)
+{
+  {
+    text += 
 "<tr>" + 
 "<td>" +
-"<button  id=\'myBtn" +    i + "\'>" +
+"<button  style=\'height: 100%; width: 100%;\'" +
+            "id=\'myBtn" +      i + "\'>" +
 "Select" +
 "</td>" +
-"<td      id=\'perkName" + i + "\'>" +
+"<td" +
+"         style=\'width: 200px\'" +
+"            id=\'perkName" +   i + "\'>" +
 perkName[i] + 
 "</td>" +
-"<td id=\'perkEffect" +    i + "\'>" +
+"<td      id=\'perkEffect" +    i + "\'>" +
 perkEffect[i] + 
 "</td>" +
-"<td id=\'perkPercent" +   i + "\'>" +
+"<td" +
+"      style=\'width: 60px\'" +
+"         id=\'perkPercent" +   i + "\'>" +
 perkPercent[i] + 
 "</td>" +
 "</tr>";
+  };
+
+  {
+    button += 
+"document.getElementById(\"myBtn" + i + "\").addEventlistener(\"click\", function() {" + 
+"first(" + i + ")}" + 
+"});";
+  };
+
 document.getElementById("myTable").innerHTML = 
 "<tr>" +
-"<th style=\'background-color: black\'></th>" +
+"<th>Click Box to Select</th>" +
 "<th>Perk</th>" +
 "<th>Effect</th>" +
 "<th>Craft%</th>" +
 "</tr>" +
 text;
-  }
-</script>
+};
+// THE TABLE IS ABOVE
+/*
+function first(p1) {
+  let a = perkName[p1];
+  document.getElementById('selectOne').innerHTML = a;
+}
+*/
+/*
+"function first(p1)  {" + 
+"a = perkName[p1];" +
+"document.getElementById(\'selectOne\').innerHTML = a;";
+document.getElementById('theFunction').innerHTML = txt;
+}
+*/
+
+
+/*
+ document.getElementById("myBtn1").addEventListener("click", function() {first(1);});
+ 
+ function first(p1) {
+  a = perkName[p1];
+  document.getElementById('selectOne').innerHTML = a;
+}
+ */
